f9367746ebd7664f754eba3d5a639ea514742d39
developer-suhag <developersuhag@gmail.com>
2021-09-11 21:45:18 +0600

    3 done

---
+++ practice.js
@@ -0,0 +1,21 @@
+// ------------------>> 3 <<-----------------------------
+/* 
+function timeOut() {
+    console.log('Hello');
+}
+
+function interval() {
+    console.log('Tik Tik Tik');
+}
+
+setTimeout(() => {
+    timeOut()
+}, 500);
+
+setTimeout(timeOut, 2000);
+
+setInterval(() => {
+    interval()
+}, 1000); */
+
+// ------------------>> 4 <<-----------------------------
